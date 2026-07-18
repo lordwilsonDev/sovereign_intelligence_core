e969add6100d3387aa688742bb57a89147d5f083
lordwilson <theapexintelligence@gmail.com>
2026-07-18 00:22:22 -0500

    Fix start.sh launch command; verify server reachable on 8766

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ fi
 export PYTHONPATH=/Users/lordwilson/msb-v2
 export MSB_REASONING_SCORER=1
 
-exec "$SELECTED_PYTHON" -m uvicorn msb_v2.api.main:create_app --factory --host "${MSB_HOST:-127.0.0.1}" --port "${MSB_PORT:-8766}" "${@:-}"
+exec "$SELECTED_PYTHON" -m uvicorn msb_v2.api.main:create_app --factory --host "${MSB_HOST:-127.0.0.1}" --port "${MSB_PORT:-8766}"
